2fc8d69e2985fec2ea54f3a13bbc6ae3ce21c32c
glennjaja <karjadidjaja@gmail.com>
2021-12-28 21:46:31 +0700

    edit seller-page

--- resources/views/seller-page.blade.php
+++ resources/views/seller-page.blade.php
@@ -5,6 +5,13 @@
 
 @section('content')
 
+    <div class="row align-items-center">
+        <p class="fs-1 pt-0 col align-self-start"> Listed Product</p>
+        <div class="col">
+            <a class="btn btn-danger float-end" href="">Add New Product</a>
+        </div>
+    </div>
+    <hr class="dropdown-divider">
 
 
 
