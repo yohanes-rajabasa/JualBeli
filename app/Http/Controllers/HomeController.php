b45b62443e0938f4697ccc02050f37356708a138
yohanesrscvt <yohanesrajabasasitorus@hotmail.com>
2022-01-17 14:14:00 +0700

    fix home page if logged as seller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     //
     public function index(){
+        // redirect if seller
+        if(Auth::user()->role_number == 2){
+            return redirect('/seller');
+        }
 
         //temp
         $products['popular'] = Product::withCount('details')->orderBy('details_count','desc')->take(8)->get();
