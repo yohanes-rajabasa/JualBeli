f24d47a6fbbeb9c99dda54d3f479e2d518af1d3f
glennjaja <karjadidjaja@gmail.com>
2022-01-17 13:31:10 +0700

    fixing seller page

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class SellerController extends Controller
 {
     public function sellerPageView(){
-        $products = Product::all();
+        $id = Auth::user()->id;
+        $products = Product::where('user_id','=',$id)->get();
+        // $products = Product::all();
         return view('/seller-page',['products'=>$products]);
     }
     
